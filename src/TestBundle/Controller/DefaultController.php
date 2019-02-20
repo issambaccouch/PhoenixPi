b9bc73098a1f50d768ef86b1ff4beb88e03d8840
issamsolid <issam.snake12@gmail.com>
2019-02-20 22:45:19 +0100

    integrate template FRONT/BACK
    fix routing && 3.4 version bugs + create blocks

---
+++ src/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@Test/Default/index.html.twig');
+    }
+}
